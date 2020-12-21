4872835a97b3d0bd59e5c4bb8379dc71fcbf5113
sarita kumari <saritakumari3087@gmail.com>
2020-12-21 17:45:25 +0530

    Test commited

--- src/test/java/com/mars/MarsAssignmentApplicationTests.java
+++ src/test/java/com/mars/MarsAssignmentApplicationTests.java
@@ -1,7 +1,9 @@
 package com.mars;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertThrows;
 
+import org.hamcrest.core.StringContains;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
@@ -14,8 +16,10 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Assert;
 
 import com.mars.controller.PersonController;
+import com.mars.exception.PersonNotFoundException;
 import com.mars.model.Person;
 
 
@@ -54,9 +58,25 @@ class MarsAssignmentApplicationTests {
 	@Test
 	public void listPersonTest() throws JSONException {
 		
-		Assertions.assertEquals(500,restTemplate.getForEntity("http://localhost:" + port + "/api/person", Person.class).getStatusCodeValue());
+		Assertions.assertEquals(200,restTemplate.getForEntity("http://localhost:" + port + "/api/person",Object.class).getStatusCodeValue());
 		
 				
 	
 	}
+	@Test
+	public void editPersonTest() {
+		Assertions.assertEquals(404, restTemplate.getForEntity("http://localhost:" + port + "/person/56",Person.class).getStatusCodeValue());
+	
+	}
+	
+	@Test
+	public void deletePersonTest() {
+		Assertions.assertEquals(404, restTemplate.getForEntity("http://localhost:" + port + "/person/56",Person.class).getStatusCodeValue());
+		String result =restTemplate.getForEntity("http://localhost:" + port + "/person/56",String.class).getBody();
+		Assertions.assertNotEquals("{\"timestamp\":\"2020-12-21T11:39:23.945+00:00\",\"status\":404,\"error\":\"Not Found\",\"message\":\"\",\"path\":\"/person/56\"}", StringContains.containsString(result));
+		 
+		
+	}
+	
+	
 }
